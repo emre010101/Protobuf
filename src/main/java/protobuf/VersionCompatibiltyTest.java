54b8b2f2b73eee2d2470d2cb034e689a3fcb5988
Emre <emrekavak3938@gmail.com>
2023-04-03 22:20:21 +0100

    LAst

--- src/main/java/protobuf/VersionCompatibiltyTest.java
+++ src/main/java/protobuf/VersionCompatibiltyTest.java
@@ -17,9 +17,19 @@ public class VersionCompatibiltyTest {
 			.setYear(2015)
 			.build();
 		
+		/*Televison television = Televison.newBuilder()
+				.setBrand("sony")
+				.setModel(2016)
+				.setType(Type.OLED)
+				.build();*/
+			
+		
 		Path pathV1 = Paths.get("tv-v1");
 		Files.write(pathV1, television.toByteArray());
-			
+		
+		/*Path pathV2 = Paths.get("tv-v2");
+		Files.write(pathV2, television.toByteArray());*/
+		
 		//Desiralising
 		byte[] bytes = Files.readAllBytes(pathV1);
 		System.out.println(
